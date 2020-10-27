210c31baa4ff92a8d990effe5c42f7a773f3c3bc
abhi-72 <chelluri.abhinav@gmail.com>
2020-10-27 14:31:48 +0530

    Asynchronous messaging to consumer

--- ms1/api/views.py
+++ ms1/api/views.py
@@ -1,20 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from nameko.standalone.rpc import ClusterRpcProxy
-
 from api.serializers import PersonSerializer
 from api.models import Person
 import json
 import os
 
 CONFIG = {'AMQP_URI': f"amqp://{os.environ.get('RABBIT_USER')}:{os.environ.get('RABBIT_PASSWORD')}@{os.environ.get('RABBIT_HOST')}"}
-print(CONFIG)
+
 
 @api_view(["GET"])
 def birds(request):
     with ClusterRpcProxy(CONFIG) as rpc:
-        result = rpc.birds.get()
-    return Response({"message": "Invoked", "data": result})
+        birds = rpc.birds.get.call_async()
+        return Response({"message": "Invoked", "data": birds.result()})
 
 def persons():
     queryset = Person.objects.all()
